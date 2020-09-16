a133f8d8682a201d6a4948c53a12a18d18e0afc4
Ramo <60301473+RamyDavin@users.noreply.github.com>
2020-09-16 02:28:43 +0200

    Update build_url.py

--- build_url.py
+++ build_url.py
@@ -4,19 +4,18 @@ from colorama import Fore
 def build_willhaben_url(self, uk):
     self.clear_console()
     while True:
-        keyword = list(input("Keyword auswählen:\n").strip())
+        keyword = list(input("Select keyword:\n").strip())
         if not keyword:
-            kw_rq = input("Möchten Sie Produkte ohne Keyword grabben? Y - Kein Keyword N - Keyword wählen\n").lower()
+            kw_rq = input("Do you want to grab products without a keyword? Y - No Keyword N - Choose Keyword\n").lower()
             if kw_rq == "y":
                 break
             elif kw_rq != "y" and kw_rq != "n":
-                print(Fore.RED + "--------------------\nUngültige Auswahl!\n--------------------" + Fore.CYAN)
+                print(Fore.RED + "--------------------\nInvalid selection!\n--------------------" + Fore.CYAN)
         else:
             break
 
-    händler_typ = "Von wem sollen Produkte gegrabbt werden?\nP - Privat\nH - Händler\nB - Beides\n"
-    types = {"p": "&ISPRIVATE=1", "h": "&ISPRIVATE=0", "b": ""}
-    typ = self.str_in_dict(händler_typ, types)
+    types = {"p": "&ISPRIVATE=1", "d": "&ISPRIVATE=0", "b": ""}
+    typ = self.str_in_dict("By whom should products be grabbed?\nD - Private dealers\nH - Business Dealers\nB - Both\n", types)
 
     to_filter = {"+": "%2B", " ": "+", "ö": "%F6", "ü": "%FC", "ä": "%E4", "Ö": "%D6", "Ü": "%DC", "Ä": "%C4", "×": "%D7",
                "#": "%23", "!": "%21", "/": "%2F", ",": "%2C", "€": "%80", "%": "%25", "=": "%3D", "(": "%28",
@@ -30,27 +29,20 @@ def build_willhaben_url(self, uk):
 
     if uk == 20:
         url = "https://www.willhaben.at/iad/kaufen-und-verkaufen/zu-verschenken/marktplatz"  f"?rows=100" + f"&keyword={keyword}"
-    elif uk != "":
-        while True:
-            mindest_preis = self.int_input("Mindestpreis:\n")
-            maximal_preis = self.int_input("Maximalpreis:\n")
-            if mindest_preis > maximal_preis:
-                print(Fore.RED + "--------------------\nUngültige Eingabe!\n--------------------" + Fore.CYAN)
-
-            else:
-                break
-        url = self.url_base + self.menu["links"][str(
-            uk)] + f"?PRICE_FROM={mindest_preis}" + f"{types[typ]}" + f"&keyword={keyword}" + f"&PRICE_TO={maximal_preis}" + f"&rows=100"
     else:
         while True:
-            mindest_preis = self.int_input("Mindest Preis:\n")
-            maximal_preis = self.int_input("Maximal Preis:\n")
-            if mindest_preis > maximal_preis:
-                print(Fore.RED + "--------------------\nUngültige Eingabe!\n--------------------" + Fore.CYAN)
+            minimum_price = self.int_input("Minimum price:\n")
+            maximum_price = self.int_input("Maximum price:\n")
+            if minimum_price > maximum_price:
+                print(Fore.RED + "---------------------------------------------------\nMinimum price cannot be higher than maximum price!\n---------------------------------------------------" + Fore.CYAN)
+
             else:
                 break
-        print(Fore.GREEN + "Bitte warten...." + Fore.CYAN)
+        print(Fore.GREEN + "Please wait..." + Fore.CYAN)
 
-        url = self.url_base + f"?PRICE_FROM={mindest_preis}" + f"{types[typ]}" + f"&keyword={keyword}" + f"&PRICE_TO={maximal_preis}" + "&rows=100"
+        if uk != "":
+            url = self.url_base + self.menu["links"][str(uk)] + f"?PRICE_FROM={minimum_price}" + f"{types[typ]}" + f"&keyword={keyword}" + f"&PRICE_TO={maximum_price}" + f"&rows=100"
+        else:
+            url = self.url_base + f"?PRICE_FROM={minimum_price}" + f"{types[typ]}" + f"&keyword={keyword}" + f"&PRICE_TO={maximum_price}" + "&rows=100"
 
     return url
